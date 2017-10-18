10f2a755b99fd2c0dba245f52f1e09f261370024
andrew <andrew@zipcoders-MacBook-Pro.local>
2017-10-18 17:06:49 -0400

    Sum of the Numbers Lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
+        int sum = 0;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        int userInput = sc.nextInt();
 
+        for (int x = 1; x <= userInput; x++) {
+            sum += x;
+        }
+
+        System.out.println("The sum of the numbers until " + userInput + " is " + sum);
     }
 }
