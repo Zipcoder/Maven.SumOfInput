0a2163f87512e4304016d8f901685c5cfda1a670
zachary <zachary@zipcoders-MacBook-Pro.local>
2017-10-18 18:34:03 -0400

    SumOfInput

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,24 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
+        int userNumber;
+        int sum = 0;
+
+        System.out.println("Enter a number!");
+        Scanner input = new Scanner(System.in);
+        userNumber = input.nextInt();
+
+        for (int i = 0; i <= userNumber; i++){
+            sum+= i;
+        }
+
+        System.out.println(sum);
+
+
+
+
 
     }
 }
