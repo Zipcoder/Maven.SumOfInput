7d0d5e41d7df2b9d7a0c1c30c298191451bb8c64
pavel <pavel@zipcoders-MacBook-Pro.local>
2017-10-18 18:23:28 -0400

    All done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Please provide a number.");
+
+        int inputNumber = input.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= inputNumber; i++ ){
+            sum += i;
+        }
+
+        System.out.printf("The final sum using number %d, is %d", inputNumber, sum);
     }
 }
