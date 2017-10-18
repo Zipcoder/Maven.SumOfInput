0d3280d514d58212c72b6e746fb89ae9c2b46708
merin j <merinj@zipcoders-MacBook-Pro-7.local>
2017-10-18 17:35:53 -0400

    adding no:

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
@@ -5,5 +7,13 @@ public class Main {
 
     public static void main(String[] args){
 
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Enter your no:");
+        int num= scan.nextInt();
+        int sum=0;
+        for(int i=1;i<=num;i++){
+           sum+=i;
+        }
+        System.out.println("Sum of the no: from "+0+"-"+num+" is "+sum);
     }
 }
