5462f1f4f99a962a207f9d0b44ca9f3974e75f56
samuel <samuel@zipcoders-MacBook-Pro.local>
2017-10-19 19:04:19 -0400

    Sum of the Numbers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
@@ -5,5 +8,24 @@ public class Main {
 
     public static void main(String[] args){
 
+        Scanner scan = new Scanner(System.in);
+        int sum = 0;
+
+        
+        System.out.println(
+                "Please enter a number ");
+        int num = scan.nextInt();
+
+
+      for (int i = 1; i <= num; i++)
+            sum += i;
+        System.out.println("Sum Of Input " + sum);
     }
 }
+
+
+
+
+
+
+
