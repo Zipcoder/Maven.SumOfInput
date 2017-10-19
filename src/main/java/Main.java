3508cd690325d2319d3b5a22c7c974553677d13d
raul <raul@zipcoders-MacBook-Pro.local>
2017-10-19 09:03:23 -0400

    SumOfNumbers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
+
+        System.out.println("Write a number and we will give you the sum from 0 to it ...");
+
+
+        SumOfInput sumOfInput = new SumOfInput();
+
+        Scanner scanner = new Scanner(System.in);
+
+        int userInput = scanner.nextInt();
+
+        sumOfInput.sumCalculation(userInput);
+
 
     }
 }
---
+++ src/main/java/SumOfInput.java
@@ -0,0 +1,14 @@
+public class SumOfInput {
+
+    public void sumCalculation(int value){
+
+        int sum = 0;
+
+        for(int i = 0;i<=value;i++){
+
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
