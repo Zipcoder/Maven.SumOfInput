f84e3de43d7df0cdbcda9e95985844bf88c682af
donald f <donaldf@zipcoders-MacBook-Pro-4.local>
2017-10-19 14:21:46 -0400

    this was a lot shorter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+
+
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
@@ -5,5 +9,17 @@ public class Main {
 
     public static void main(String[] args){
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("This is Gaussian Wizardry. Please enter a number to receive your sum.");
+        int num = scan.nextInt();
+        int sum = 0;
+
+
+
+        for (int i=1; i <= num; i++) {
+            sum += i;
+        }
+        System.out.println("\nThe sum of your number using Gaussian Wizardry is " + sum + ".");
     }
 }
