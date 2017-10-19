6796644e18b1eafc82c748945b3b57c6780bf1e3
abrar <abrar@zipcoders-MBP.fios-router.home>
2017-10-18 23:27:58 -0400

    did the assignment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
+ * This class prints the sum of values from 1 to user input number
  */
 public class Main {
 
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert any integer: ");
+        double input = scanner.nextInt();    // save user input
+        double sum = 0;
+        for (double i=1; i<=input; i++){
+            sum+=i;   // add the numbr i into sum untill the number is equal to user input
+        }
 
+        System.out.println("The sum is " + sum);
     }
 }
