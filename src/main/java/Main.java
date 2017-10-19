e64495b98afff5f051aa105033ce6a95d9ce7a39
ryan <ryan@zipcoders-MacBook-Pro.local>
2017-10-18 20:54:31 -0400

    Finished Lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input any number");
+        int n = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i<= n; i++){
+            sum += i;
+
+        }
+        System.out.println("Sum of the numbers, 1 to \"Input\": " + sum);
 
     }
 }
