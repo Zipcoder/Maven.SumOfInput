206363d0c70906d95e02ccace4ae7b2feb72e5da
aaron <aaron@zipcoders-MacBook-Pro.local>
2017-10-19 15:45:12 -0400

    Aaron H. Sum of Input Lab Submission

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+package com.company;
+
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args){
+
+
+    public static void main(String[] args) {
+	System.out.println("Greetings, please enter a number below:");
+            Scanner userInput = new Scanner(System.in);
+
+        long calcSum = 0;
+        long userNumber = userInput.nextLong();
+
+    for (int i = 0; i <= userNumber; i++) {
+
+        calcSum += i;
+
+    }
+
+        System.out.println(calcSum);
 
     }
 }
