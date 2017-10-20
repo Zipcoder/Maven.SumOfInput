9a58688af7a2d3c1e9b28b6bf1e1b722540ad063
zan <zan@zipcoders-MacBook-Pro.local>
2017-10-20 18:17:37 -0400

    done with sum of input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Tell me a number and I'll add it!");
+        int inputNumber = scanner.nextInt();
+        System.out.println("");
+        int results = addition(inputNumber);
+        System.out.println("I added it all up! Your sum is " + results);
+    }
+
+    public static int addition(int input) {
+        int sum = 0;
+        for(int x = 1; x <= input; x++ ){
+            //System.out.println(x + "");
+            sum = sum + x;}
+
+        return sum;
 
     }
 }
