540e79602c9534e4969cdac5411ac7d4beb2a948
lina <lina@zipcoders-MacBook-Pro.local>
2017-10-22 13:48:49 -0400

    SumOfProduct

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
+import java.util.*;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-
+//        Write a program which prompts the user to input a number, n.
+        System.out.println("Please enter a number");
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int sum = 0;
+    for(int i=1; i<=n;i++){
+        sum += i;
+    }
+//        The program should respond by printing the sum of the numbers 1 to n.
+        System.out.println("The sum of 1 to "+n+" is:"+ sum);
     }
 }
