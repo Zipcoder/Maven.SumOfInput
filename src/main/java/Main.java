46b7822adc0e84849b4eb11671bec6efd6705ded
andrea <andrea@zipcoders-MacBook-Pro-2.local>
2017-10-22 12:30:42 -0400

    finished sumofinput

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        //prompt the user
+        //test
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int userNumber = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= userNumber; i++) {
+            sum += userNumber;
+            System.out.println(sum);
+        }
 
     }
 }
