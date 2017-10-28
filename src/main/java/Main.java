3b16b5bbb1ba65ccbc1cd93a18c4cc4e6647b07e
andrea <andrea@zipcoders-MacBook-Pro-2.local>
2017-10-28 18:06:01 -0400

    re-did SumOfInput lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
+//    Write a program which prompts the user to input a number, n.
+//    The program should respond by printing the sum of the numbers 1 to n.
+//    Constraint: No Guassian wizardry !
 
     public static void main(String[] args) {
-        //prompt the user
-        //test
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number");
-        int userNumber = scanner.nextInt();
+        System.out.println("Please enter a number.");
+        int userInput = scanner.nextInt();
         int sum = 0;
 
-        for (int i = 1; i <= userNumber; i++) {
-            sum += userNumber;
+        for (int i = 1; i <= userInput; i++) {
+            sum += userInput;
             System.out.println(sum);
         }
-
     }
 }
