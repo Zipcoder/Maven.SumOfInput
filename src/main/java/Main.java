6f0512307aca12bec2c8a81461f7e318d6065e1f
April Rivera <aprilrivera@zipcoders-MacBook-Pro-2.local>
2018-02-07 16:23:56 -0500

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,24 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        Scanner makeMoney = new Scanner(System.in);
+
+        try
+        { System.out.println("Enter any number.");
+            int whatChaGot = makeMoney.nextInt();
+            System.out.println("Now you've got $" + (whatChaGot + 1) + " bro");
+
+        }
+        catch(InputMismatchException exception){
+            System.out.println("I said a number, bro.");
+        }
 
     }
 }
