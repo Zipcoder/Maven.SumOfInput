00d2d7b8717c0da477e7fefa8314ad14fc58d0d8
Bo Lee <bolee@zipcoders-mbp.lan>
2018-02-06 23:29:18 -0500

    Sum of Input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int userNum = userInput.nextInt();
+        int sumOfNum = 0;
+        for (int i = 1; i <= userNum; i++) {
+            sumOfNum = sumOfNum + i;
+        }
+        System.out.println(sumOfNum);
     }
 }
