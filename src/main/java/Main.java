e6dcffb41691d262b19d3329bdca951dccfd31cf
April Rivera <aprilrivera@zipcoders-MacBook-Pro-2.local>
2018-02-07 16:44:27 -0500

    added condition for positive number

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,13 +8,20 @@ public class Main {
 
     public static void main(String[] args) {
 
+
         Scanner makeMoney = new Scanner(System.in);
 
         try
         { System.out.println("Enter any number.");
-            int whatChaGot = makeMoney.nextInt();
-            System.out.println("Now you've got $" + (whatChaGot + 1) + " bro");
 
+            int whatChaGot = makeMoney.nextInt();
+            boolean n = whatChaGot >= 0;
+
+            if (n) {
+                System.out.println("Now you've got $" + (whatChaGot + 1) + " bro");
+            } else {
+                System.out.println("Try a positive number.");
+            }
         }
         catch(InputMismatchException exception){
             System.out.println("I said a number, bro.");
