e8ff842a6e92bffef6894a3e4ceeb62dd35ebda9
Vincent Gasbarro <vincentgasbarro@zipcoders-MacBook-Pro.local>
2018-02-07 10:58:15 -0500

    edited sum of input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,29 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+
+        int sumOfIn = 0;
+
+        System.out.println("Please enter a number.");
+        int n = in.nextInt();
+
+        while (n > 0) {
+            sumOfIn += n;
+            n--;
+
+        }
+
+        System.out.println(sumOfIn);
+
+
+
 
     }
 }
