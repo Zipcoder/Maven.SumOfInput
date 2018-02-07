72164458414ecf3ecae7062e2f180ac0cdd9da45
Garrett Arant <garrettarant@MacBook-Pro-4.fios-router.home>
2018-02-06 22:35:40 -0500

    Garrett Completed Lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number;
+        int sum = 0;
+
+        System.out.println("What is your number?");
+
+        number = scanner.nextInt();
+
+        for (int i = 1; i <= number; i++ ){
+                sum += i;
+
+        }
+
+        System.out.println(sum);
+
+
+
+
+
+
 
     }
 }
