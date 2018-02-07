87911e1d3873e53cee1558078850fa3f973ed29c
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-02-07 10:52:29 -0500

    Sum of input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,17 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+import java.util.*;
 public class Main {
-
     public static void main(String[] args){
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a whole number: ");
+
+        int input = scan.nextInt();
+        int sum = 0;
+
+        for(int i = 0; i <= input; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
     }
 }
