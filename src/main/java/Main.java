fa13133ceafaff5822649c04e4cac57beb2ef25b
ghost1497 <rfrankie04@gmail.com>
2018-02-06 21:33:36 -0500

    lets try this again

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
 /**
- * Created by iyasuwatts on 10/17/17.
+ * Created by Frankie!
  */
+
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an integer to begin: ");
+        int n = input.nextInt();
+
+        System.out.println("Result: " + (1+n));
 
     }
 }
