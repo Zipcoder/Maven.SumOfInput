d6ddbbd82886f77493b5613c5b9b82212656655e
Joe Hendricks <joehendricks@zipcoders-MacBook-Pro-92.local>
2018-02-06 22:55:03 -0500

    Sum of Input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a number to add to 1: ");
+
+        int userNumber = scan.nextInt() + 1;
+
+        System.out.println("Your total is: " + userNumber);
 
     }
 }
