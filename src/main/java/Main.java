a1a0b9c1bb63720ed9d2a066c319c9f9166c3085
Madeline Bowe <madelinebowe@zipcoders-MacBook-Pro-2.local>
2018-02-07 10:12:35 -0500

    First Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
@@ -5,5 +7,18 @@ public class Main {
 
     public static void main(String[] args){
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:  ");
+        int userNum = sc.nextInt();
+        int sumNum = 0;
+
+        for (int i = 0; i <= userNum; i++) {
+
+            sumNum += i;
+            System.out.println(sumNum);
+
+        }
+
     }
+
 }
