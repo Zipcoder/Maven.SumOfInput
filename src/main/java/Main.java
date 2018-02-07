3e491baf5649b9de995522d22820ba11ad1013bd
Dylan Camac <dylancamac@zipcoders-MacBook-Pro-5.local>
2018-02-07 16:12:26 -0500

    i feel super good

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,24 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+import java.util.Scanner;
+
 public class Main {
 // this is mine now!
     public static void main(String[] args){
 
+        Scanner in = new Scanner(System.in);
+
+ // lets get that input
+        System.out.println("What is your number?");
+        int input = in.nextInt();
+        int newInput = 0;
+
+        for (int i = 0; i <= input; i++) {
+            newInput = newInput + i;
+        }
+        System.out.println("Your total is " + newInput);
     }
 }
