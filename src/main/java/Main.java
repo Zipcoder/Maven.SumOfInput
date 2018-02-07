cb79c22412c5dd10e831bd05d7fd863640f601ab
Katrice Williams-Dredden <katricewilliams-dredden@zipcoders-MacBook-Pro.local>
2018-02-07 16:07:53 -0500

    finished lab using a for loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
 
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = scan.nextInt();
+
+        int answer = 0;
+        for (int i =1; i <= num; i++) {
+            answer = answer + i;
+        }
+        System.out.println(answer);
     }
 }
