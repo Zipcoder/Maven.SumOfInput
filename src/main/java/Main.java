ba50c95c4cc792d0250c20dddd3fb7dbad5b82d3
Peter McCormick <petermccormick@zipcoders-MacBook-Pro-109.local>
2018-02-07 15:22:01 -0500

    Sum of Numbers Tested an Complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("What is your number? ");
+        int inputValue = in.nextInt();
+
+        int sumOfNumbers = 0;
+        for (int i = 1; i<= inputValue; i++) sumOfNumbers +=i;
+
+
+
+        System.out.println
+                ("Your number: " + inputValue + " results in a sum of " + sumOfNumbers);
+
+
+
+
 
     }
 }
