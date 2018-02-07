60f8bb2d368335977d44037fa724dc01c7a4f9de
Amy Gill <amygill@MacBook-Pro-2.local>
2018-02-07 15:54:27 -0500

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number.");
+        int response = input.nextInt();
+        int count = 1;
+        for (int i = 0; i < response; i++){
+            count += i;
+        }
+
+        System.out.println(count);
 
     }
 }
