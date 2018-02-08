c996687169303097a8e4b0ab0679ae83fd3e6174
Owen Murphy <owenmurphy@zipcoders-MacBook-Pro-3.local>
2018-02-07 20:44:49 -0500

    money, money, money

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Insert Number Here");
+
+    int test = in.nextInt();
+
+    int result = 0;
+
+    for(int i=0;i <=test; i++){
+
+        result+=i;
+        System.out.println(result);
+
+    }
+
+
+
+
+
+
+
 
     }
 }
