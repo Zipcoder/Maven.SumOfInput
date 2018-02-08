8958e33819e3bc06e09692357257a8dc5cf2a984
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-2.local>
2018-02-07 19:59:58 -0500

    complete lab on Sum of Input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,17 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.*;
 public class Main {
 
     public static void main(String[] args){
-
+        int sum =0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number!");
+        int inputNumber = input.nextInt();
+        for(int i=1;i<=inputNumber;i++){
+            sum+=i;
+        }
+        System.out.println(sum);
     }
 }
