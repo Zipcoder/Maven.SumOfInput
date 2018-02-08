d3e62a3169ffe8b09e6a711cd74284dc5dd7988d
Jordan Elderidge <jordanelderidge@zipcoders-MacBook-Pro.local>
2018-02-07 20:45:35 -0500

    Project done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Insert Number here");
+
+        int test = in.nextInt();
+
+        int result = 0;
+
+
+        for (int i=0;i <=test; i++){
+            result +=i;
+            System.out.println(result);
+        }
+
+
+
+
+
 
     }
 }
