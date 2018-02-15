0ad6c1314317ba41bc4782b426687562e305fb9e
ghost1497 <rfrankie04@gmail.com>
2018-02-15 16:09:38 -0500

    can not believe i did that...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,8 +9,12 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter an integer to begin: ");
         int n = input.nextInt();
+        int result=0;
+        for(int i = 1; i <= n; i++){
+            result += i;
+        }
 
-        System.out.println("Result: " + (1+n));
+        System.out.println("Result: " + result);
 
     }
 }
