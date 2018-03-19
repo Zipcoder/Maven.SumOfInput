0e9c65d748bf49d4639612d4fb93228bdaa1b34f
Dominique Clarke <dominiquedclarke@gmail.com>
2018-03-19 11:18:41 -0400

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number");
+
+        int n = Integer.parseInt(input.nextLine());
+        int i = 1;
+
+        while(i <= n) {
+            System.out.println(i);
+            i++;
+        }
     }
 }
