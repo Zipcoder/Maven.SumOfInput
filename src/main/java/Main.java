10867d8a0e349a8b2a6ec0a0c266c0dd5261bec6
Dominique Clarke <dominiquedclarke@gmail.com>
2018-03-19 11:45:05 -0400

    added check for integer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,17 +6,49 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        promptUser(true);
+    }
 
+    public static void promptUser(boolean firstPrompt) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number");
 
-        int n = Integer.parseInt(input.nextLine());
-        int i = 1;
+        if(firstPrompt)
+        {
+            System.out.println("Enter a number");
+        }
 
-        while(i <= n) {
-            System.out.println(i);
-            i++;
+        String s = input.nextLine();
+
+        if(isInteger(s))
+        {
+            int n = Integer.parseInt(s);
+            int i = 1;
+
+            while(i <= n)
+            {
+                System.out.println(i);
+                i++;
+            }
+        }
+
+        else
+        {
+            System.out.println("Please enter an integer");
+            promptUser(false);
+        }
+    }
+
+    public static boolean isInteger(String s) {
+        try
+        {
+            Integer.parseInt(s);
+            return true;
+        }
+        catch (NumberFormatException ex)
+        {
+            return false;
         }
     }
 }
+
